a465fbad1367375a9e3d3ceb98f1cabb1044a1b1
Jeremías <jeremiias122002@gmail.com>
2023-08-04 12:51:35 +0200

    new files .TS

---
+++ carpeta_nueva/sec2/tupla.ts
@@ -0,0 +1,18 @@
+let vector: [number, number];
+let vector2: [number, number];
+
+vector = [3,6]
+vector2 = [5,10]
+
+// sume vector
+
+let vector_resultante: [number, number] = [vector[0] + vector2[0], vector[1] + vector2[1]]
+
+console.log(vector_resultante);
+
+let tupla_5_element: [number, number, number, number, number] = [2,4,6,8,10]
+
+let profesor: [string, number] = ["Jere Ferman", 1.80]
+let estudiante: [string, number] = ["Valentín Oliva", 1.76]
+
+console.log(`${estudiante[0]} mide ${estudiante[1]} metros`)
